3a19d4f04d9687c59c028ac171d3c1bfc1b73d42
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2025-11-17 22:18:36 +0900

    feat: 백준 1463번 / 1로 만들기

---
+++ src/main/java/j2025/B1463.java
@@ -0,0 +1,43 @@
+package j2025;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 백준 1463번 : 1로 만들기 - 시간 초과 ㅜ (dp Top-down)
+public class B1463 {
+    static int[] dp = new int[1000001];
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        dp[0] = 0;
+        dp[1] = 0;
+        dp[2] = 1;
+        dp[3] = 1;
+
+        System.out.println(getMinVal(n));
+
+    }
+
+    private static int getMinVal(int n) {
+        if (dp[n] != 0) {
+            return dp[n];
+        }
+        int min_val = getMinVal(n-1) + 1;
+
+        if (n%2 == 0) {
+            int two = getMinVal(n/2) + 1;
+            min_val = Math.min(min_val, two);
+        }
+
+        if (n%3 == 0) {
+            int three = getMinVal(n/3) + 1;
+            min_val = Math.min(min_val, three);
+        }
+
+        dp[n] = min_val;
+        return dp[n];
+    }
+
+}
---
+++ src/main/java/j2025/B1463_2_s.java
@@ -0,0 +1,32 @@
+package j2025;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 백준 1463번 : 1로 만들기 (dp bottom-up)
+public class B1463_2_s {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        int[] dp = new int[n + 1];
+
+        dp[1] = 0;
+
+        for (int i = 2; i <= n; i++) {
+
+            dp[i] = dp[i - 1] + 1;
+
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 2] + 1);
+            }
+
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
