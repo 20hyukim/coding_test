98001f4ff064707f1ab20646cc110feb29300f8b
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2025-11-25 22:48:15 +0900

    feat: 백준 15721번 / 번데기

---
+++ src/main/java/j2025/B15721.java
@@ -0,0 +1,60 @@
+package j2025;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+// 백준 15721번 : 번데기
+public class B15721 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int a = Integer.parseInt(br.readLine());
+        int t = Integer.parseInt(br.readLine());
+
+        int type = Integer.parseInt(br.readLine());
+        int[] calc = new int[2];
+
+
+        int dup = 2;
+        int count = 0;
+        INIT: while (calc[type] != t) {
+            for (int i = 0; i < 4; i++) {
+                count ++;
+                if (i % 2 == 0) {
+                    calc[0] ++;
+                    if (type == 0 && t == calc[type]) {
+                        break INIT;
+                    }
+                } else {
+                    calc[1] ++;
+                    if (type ==1 && t == calc[type]) {
+                        break INIT;
+                    }
+                }
+            }
+
+            for (int i = 0; i < dup; i ++) {
+                count ++;
+                calc[0] ++;
+                if (type == 0 && t == calc[type]) {
+                    break INIT;
+                }
+            }
+
+            for (int i = 0; i < dup; i++) {
+                count ++;
+                calc[1] ++;
+                if (type ==1 && t == calc[type]) {
+                    break INIT;
+                }
+            }
+            dup++;
+
+        }
+
+        System.out.println((count-1) % a);
+
+    }
+
+}
