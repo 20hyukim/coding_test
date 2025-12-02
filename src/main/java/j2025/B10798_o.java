2891a158a757a6151a37de8a80b0020707ae8f37
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2025-12-02 21:51:05 +0900

    feat: 백준 10798번 : 세로읽기

---
+++ src/main/java/j2025/B10798_o.java
@@ -0,0 +1,32 @@
+package j2025;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class B10798_o {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        String[] words = new String[5];
+
+        int max_len = 0;
+
+        for (int i = 0; i < 5; i++) {
+            words[i] = br.readLine();
+            max_len = Math.max(max_len, words[i].length());
+        }
+
+        for (int l = 0; l < max_len; l++) {
+            for (int w = 0; w < 5; w++) {
+
+                if (words[w].length() > l) {
+                    sb.append(words[w].charAt(l));
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
