479a29f0332a7928f79bfdadf387f6d0a92f204a
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2025-12-08 23:05:06 +0900

    feat: 백준 11653번 / 소인수분해

---
+++ src/main/java/j2025/B17521.java
@@ -0,0 +1,39 @@
+package j2025;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+// 백준 17521번 : Byte Coin
+public class B17521 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        long money = Long.parseLong(st.nextToken());
+        long coin = 0;
+
+        int[] price = new int[n];
+        for (int i = 0; i < n; i++) {
+            price[i] = Integer.parseInt(br.readLine());
+        }
+
+        for (int i = 0; i < n - 1; i++) {
+            if (price[i+1] > price[i]) {
+                long buy = money / price[i];
+                coin += buy;
+                money -= buy * price[i];
+            } else if (price[i+1] < price[i]) {
+                money += coin * price[i];
+                coin = 0;
+            }
+        }
+
+        // 최소 때, 매수를 해놨으니까
+        money += coin * price[n-1];
+
+        System.out.println(money);
+    }
+}
