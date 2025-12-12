73bffa4ad84a8e9e284556e6165321d61f8da899
미뿌감 <80188808+20hyukim@users.noreply.github.com>
2025-12-12 23:48:21 +0900

    feat: 백준 2581번 / 소수

---
+++ src/main/java/j2025/B2581.java
@@ -0,0 +1,45 @@
+package j2025;
+
+import java.io.*;
+
+// 백준 2581번 : 소수
+public class B2581 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int m = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+
+        int total = 0;
+        int minVal = 0;
+
+        for (int i = m; i < n+1; i++) {
+            if (isPrime(i)) {
+                total += i;
+                if (total == i) {
+                    minVal = i;
+                }
+            }
+        }
+
+        if (total == 0) {
+            bw.write(String.valueOf(-1));
+        } else {
+            bw.write(total + "\n");
+            bw.write(String.valueOf(minVal));
+        }
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+
+    private static boolean isPrime(int n) {
+        if (n==1) return false;
+        int rooted = (int)Math.sqrt(n) + 1;
+        for (int i = 2; i < rooted; i++) {
+            if ( n % i == 0) return false;
+        }
+        return true;
+    }
+}
